73443322690f9481e450a194202d4620dcb8df19
programmer-amatir <soklinliquid1234@gmail.com>
2024-01-14 10:13:43 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=7">
+    </head>
+    <body>
+        <h1>hello world</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, iure quibusdam harum tenetur maxime dolorum quae sunt quos aspernatur quod nulla cum odit obcaecati hic ea suscipit ab similique repellendus.</p>
+    </body>
+</html>
